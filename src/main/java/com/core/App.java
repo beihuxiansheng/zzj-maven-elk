c0c37031091649267092c9c0065045ac53bc880e
zhijie.zhang@baozun.com <zhijie.zhang@baozun.com>
2018-07-17 16:05:58 +0800

    20180717

--- src/main/java/com/core/App.java
+++ src/main/java/com/core/App.java
@@ -1,6 +1,10 @@
 package com.core;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.elasticsearch.client.AdminClient;
 import org.elasticsearch.action.ActionFuture;
@@ -9,15 +13,28 @@ import org.elasticsearch.action.get.GetResponse;
 import org.springframework.boot.SpringApplication;
 import org.elasticsearch.client.IndicesAdminClient;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.get.GetRequestBuilder;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.elasticsearch.client.transport.TransportClient;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.core.bean.PageBean;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
@@ -66,10 +83,11 @@ public class App {
 		} else {
 			System.out.println("索引不存在...");
 
-			CreateIndexRequestBuilder createIndexRequestBuilder = indicesAdminClient.prepareCreate(indexName);
-			ActionFuture<CreateIndexResponse> actionFutureCreate = createIndexRequestBuilder.execute();
-			CreateIndexResponse createIndexResponse = actionFutureCreate.actionGet();
-			
+			//CreateIndexRequestBuilder createIndexRequestBuilder = indicesAdminClient.prepareCreate(indexName);
+			//ActionFuture<CreateIndexResponse> actionFutureCreate = createIndexRequestBuilder.execute();
+			//CreateIndexResponse createIndexResponse = actionFutureCreate.actionGet();
+			CreateIndexResponse createIndexResponse = indicesAdminClient.prepareCreate(indexName).execute().actionGet();
+
 			return createIndexResponse.isAcknowledged();
 		}
 	}
@@ -99,7 +117,13 @@ public class App {
 
 			mappingBuilder.startObject("id").field("type", "keyword").field("store", Boolean.TRUE).endObject()
 						  .startObject("res_no").field("type", "text").field("store", Boolean.TRUE).endObject()
-						  .startObject("title").field("type", "text").field("store", Boolean.TRUE).endObject()
+
+						  .startObject("title").field("type", "text")
+						  .field("index_analyzer", "ik_max_word")
+						  .field("search_analyzer", "ik_max_word")
+						  .field("store", Boolean.TRUE)
+						  .endObject()
+
 						  .startObject("cover_pic_id").field("type", "long").field("store", Boolean.TRUE).endObject()
 						  .startObject("source_id").field("type", "long").field("store", Boolean.TRUE).endObject()
 						  .startObject("res_attr").field("type", "text").field("store", Boolean.TRUE).field("fielddata", Boolean.TRUE).endObject()
@@ -200,11 +224,14 @@ public class App {
 
 
 
-
-	@GetMapping("/getData")
+	/**
+	 * 根据ID查询数据
+	 * 
+	 */
+	@GetMapping("/getDataById")
 	@ResponseBody
-	public ResponseEntity getData(@RequestParam(name = "id", defaultValue="") String id) {
-		GetRequestBuilder getRequestBuilder = transportClient.prepareGet("indexName", "type", "id");
+	public ResponseEntity getData(@RequestParam(name = "index") String index, @RequestParam(name = "type") String type, @RequestParam(name = "id") String id) {
+		GetRequestBuilder getRequestBuilder = transportClient.prepareGet(index, type, id);
 		GetResponse getResponse = getRequestBuilder.get();
 
 		if(!getResponse.isExists()) {
@@ -215,6 +242,140 @@ public class App {
 	}
 
 
+
+
+
+
+
+	/**
+	 * 分页查询数据
+	 * 
+	 */
+	@GetMapping("/searchDataPage")
+	@ResponseBody
+	public PageBean searchDataPage(@RequestParam(name = "index") String index, @RequestParam(name = "type") String type) {
+		
+		int startPage = 1;
+		int pageSize = 10;
+		String[] fields = {"title"};
+		String sortField = null;
+		String highlightField = null;
+
+		//
+		QueryBuilder query = QueryBuilders.matchQuery("title", "球星拍成");
+
+		// 创建查询请求
+        SearchRequestBuilder searchRequestBuilder = transportClient.prepareSearch(index);
+
+        searchRequestBuilder.setTypes(type);
+        searchRequestBuilder.setSearchType(SearchType.QUERY_THEN_FETCH);
+
+        // 需要显示的字段，逗号分隔（缺省为全部字段）
+        /*if (fields != null && fields.contains(",")) {
+            searchRequestBuilder.setFetchSource(fields.split(","), null);
+        }*/
+        searchRequestBuilder.setFetchSource(fields, null);
+
+        // 排序字段
+        if (sortField != null) {
+            searchRequestBuilder.addSort(sortField, SortOrder.DESC);
+        }
+
+        // 高亮（xxx=111,aaa=222）
+        if (highlightField != null) {
+            HighlightBuilder highlightBuilder = new HighlightBuilder();
+
+            //highlightBuilder.preTags("<span style='color:red' >");     //设置前缀
+            //highlightBuilder.postTags("</span>");                      //设置后缀
+
+            // 设置高亮字段
+            highlightBuilder.field(highlightField);
+            searchRequestBuilder.highlighter(highlightBuilder);
+        }
+
+        //searchRequestBuilder.setQuery(QueryBuilders.matchAllQuery());
+        searchRequestBuilder.setQuery(query);
+
+        // 分页应用
+        searchRequestBuilder.setFrom(startPage).setSize(pageSize);
+
+        // 设置是否按查询匹配度排序
+        searchRequestBuilder.setExplain(true);
+
+        // 执行搜索,返回搜索响应信息
+        SearchResponse searchResponse = searchRequestBuilder.execute().actionGet();
+
+        long totalHits = searchResponse.getHits().totalHits;
+        long length = searchResponse.getHits().getHits().length;
+
+        System.out.println("共查询到" + totalHits + "条数据,处理数据条数" + length);
+
+        if (searchResponse.status().getStatus() == 200) {
+        	// 解析对象
+            List<Map<String, Object>> sourceList = setSearchResponse(searchResponse, highlightField);
+
+            return new PageBean(startPage, pageSize, (int) totalHits, sourceList);
+        } else {
+        	return null;
+        }
+
+    }
+
+
+
+
+
+	/**
+     * 高亮结果集 特殊处理
+     *
+     */
+    private static List<Map<String, Object>> setSearchResponse(SearchResponse searchResponse, String highlightField) {
+        List<Map<String, Object>> sourceList = new ArrayList<Map<String, Object>>();
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for(SearchHit searchHit : searchResponse.getHits().getHits()) {
+            searchHit.getSourceAsMap().put("id", searchHit.getId());
+
+            if (highlightField != null) {
+                System.out.println("遍历 高亮结果集，覆盖 正常结果集" + searchHit.getSourceAsMap());
+                Text[] text = searchHit.getHighlightFields().get(highlightField).getFragments();
+                if (text != null) {
+                	for (Text str : text) {
+                        stringBuffer.append(str.string());
+                    }
+                	//遍历 高亮结果集，覆盖 正常结果集
+                    searchHit.getSourceAsMap().put(highlightField, stringBuffer.toString());
+                }
+            }
+            sourceList.add(searchHit.getSourceAsMap());
+        }
+
+        return sourceList;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
